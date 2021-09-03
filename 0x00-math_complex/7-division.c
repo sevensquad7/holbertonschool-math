810b1d2cb64477a44b0fb9e06f657b8f5cec54e8
zye7ert <edgarxot@gmail.com>
2021-09-03 08:16:45 -0500

    update task

---
+++ 0x00-math_complex/7-division.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <math.h>
+
+/**
+* division - return the division
+* @c1: complex
+* @c2: complex
+* @c3: complex pointer
+* Return: void
+*/
+
+void division(complex c1, complex c2, complex *c3)
+{
+
+double a = c1.re;
+double b = c1.im;
+
+double c = c2.re;
+double d = c2.im;
+
+c3->re = ((a * c) + (b * d)) / (pow(c, 2) + pow(d, 2));
+c3->im = ((b * c) - (a * d)) / (pow(c, 2) + pow(d, 2));
+}
